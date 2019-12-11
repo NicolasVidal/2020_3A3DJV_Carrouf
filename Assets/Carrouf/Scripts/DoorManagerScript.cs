a9b78cd43f233cf7a42600d3e6edc9479120ee96
Nicolas Vidal <pro@nicolasvidal.fr>
2019-12-11 09:46:10 +0100

    Scene Setup

---
+++ Assets/Carrouf/Scripts/DoorManagerScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Carrouf.Scripts
+{
+    public class DoorManagerScript : MonoBehaviour
+    {
+        [SerializeField]
+        private TriggerDispatcherScript doorDetectionZoneTriggerDispatcher;
+
+        [SerializeField]
+        private Animator doorAnimator;
+
+        private static readonly int IsOpened = Animator.StringToHash("IsOpened");
+
+        private void Awake()
+        {
+            doorDetectionZoneTriggerDispatcher.TriggerEntered += _ => doorAnimator.SetBool(IsOpened, true);
+            doorDetectionZoneTriggerDispatcher.TriggerExited += _ => doorAnimator.SetBool(IsOpened, false);
+        }
+    }
+}
